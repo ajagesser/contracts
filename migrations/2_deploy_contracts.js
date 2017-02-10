55f854cd078f562ed8761c91f817396e36eae4d1
Arvind Jagesser <arvind@milvum.com>
2017-02-10 20:56:10 +0100

    typos

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
-var StemBiljetBeheerder = artifacts.require("./StemBiljetBeheerder.sol");
+var StembiljetBeheerder = artifacts.require("./StembiljetBeheerder.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
-  deployer.deploy(StemBiljetBeheerder);
+  deployer.deploy(StembiljetBeheerder);
 };
