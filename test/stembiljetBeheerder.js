4e68f33ecc5f0beb6e9e416466f86c5c51eb4966
Arvind Jagesser <arvind@milvum.com>
2017-02-11 13:22:06 +0100

    Unit test

--- test/stembiljetBeheerder.js
+++ test/stembiljetBeheerder.js
@@ -1,2 +1,17 @@
-var StemBiljetBeheerder = artifacts.require("./StemBiljetBeheerder.sol");
+var StembiljetBeheerder = artifacts.require("./StembiljetBeheerder.sol");
+
+var voteTokenGlobal = "0xbbdef80000000000000000000000000000000000000000000000000000000000";
+
+contract('StembiljetBeheerder', function(accounts) {
+
+  it("should be able to insert votes", function() {
+      return StembiljetBeheerder.deployed().then(function(instance) { 
+           instance.registerVoteToken(voteTokenGlobal, "0xbbded0000000000000000000000000000000000000000000000000000000000");
+           return instance.getVoteTokens();
+        }).then(function(voteToken) {
+            assert.equal(voteToken[0], 0xbbded00000000000000000000000000000000000000000000000000000000000, "Token was registered");
+        })
+  });
+});
+
 
